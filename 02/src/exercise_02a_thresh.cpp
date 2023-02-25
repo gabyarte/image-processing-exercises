3b673130255449e4377ff599c7ee8db363ea85f9
Pablo Bande <pablobande200@gmail.com>
2023-02-25 19:01:07 +0100

    feat: exercise02 completed

--- 02/src/exercise_02a_thresh.cpp
+++ 02/src/exercise_02a_thresh.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-int main() {
-  // cv::Mat gImg1 = cv::imread()
+cv::Mat process_threshold(cv::Mat* img, int threshold) {
+  cv::Mat output_img = img->clone();
+  for (int i = 0; i < img->rows; i++) {
+    for (int j = 0; j < img->cols; j++) {
+      output_img.at<uchar>(j, i) = (img->at<uchar>(j, i) >= threshold) ? 255 : 0;
+    }
+  }
+  return output_img;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    std::cout << "Error: number of arguments: include input image file" << std::endl;
+    return 1;
+  }
+  std::string data_path = "data/";
+  cv::Mat img1 = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+  int threshold = std::stoi(argv[2]);
+
+  cv::Mat output_img = process_threshold(&img1, threshold);
+
+  cv::imwrite(data_path + "output1.pgm", output_img);
+  cv::imshow("Window: Output", output_img);
+  // cv::waitKey(0);
 }
---
+++ 02/src/exercise_02b_compare.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+bool compare_images(cv::Mat* img1, cv::Mat* img2) {
+  if (img1->rows != img2->rows || img1->cols != img2->cols)
+    return false;
+
+  for (int i = 0; i < img1->rows; i++) {
+    for (int j = 0; j < img1->cols; j++) {
+      if (img1->at<uchar>(j, i) != img2->at<uchar>(j, i))
+        return false;
+    }
+  }
+  return true;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    std::cout << "Error: number of arguments: include input image file" << std::endl;
+    return 1;
+  }
+  std::string data_path = "data/";
+  cv::Mat img1 = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+  cv::Mat img2 = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
+
+  std::ofstream output;
+  output.open("data/exercise_02b_output_01.txt");
+  output << compare_images(&img1, &img2);
+  output.close();
+  std::cout << compare_images(&img1, &img2);
+}
