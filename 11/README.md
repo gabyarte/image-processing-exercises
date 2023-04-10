f1f38a9f77ae9654b54d6652748eefd53e40b039
Pablo Bande <pablobande200@gmail.com>
2023-04-10 19:18:32 +0100

    feat: Flat zone implemented

--- 11/README.md
+++ 11/README.md
@@ -1,14 +1,22 @@
-# Instructions
+# UPM Master's in Data Science: Exercises for Image Processing, Analysis and Classification subject
 
-## Compilation
+## Collaborators
+* Pablo Bande Sánchez - Girón ([@pbande](https://github.com/pbande))
+* Gabriela Argüelles Terrón ([@gabyarte](https://github.com/gabyarte))
+
+## Instructions
+
+### Compilation
 
 ```bash
 cmake .
 make
 ```
 
-## Run
+### Run
 
 ```bash
+./bin/exercise_11a data/test_1.txt data/immed_gray_inv_20051218_frgr4.pgm output/test_1.pgm 
 
+./bin/exercise_11a data/test_2.txt data/gran01_64.pgm output/test_2.pgm 
 ```
--- 11/src/exercise_11a.cpp
+++ 11/src/exercise_11a.cpp
@@ -1,21 +1,56 @@
+#include <fstream>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <queue>
 
-cv::Mat something(cv::Mat* img) {
-  cv::Mat output_img = img->clone();
+#include "../../include/functions.cpp"
 
-  return output_img;
+void read_input(std::string input_file_name, int& x, int& y, int& connectivity, int& flat_zone_label) {
+  std::ifstream input(input_file_name);
+
+  if (input.is_open()) {
+    input >> x;
+    input >> y;
+    input >> connectivity;
+    input >> flat_zone_label;
+    input.close();
+  } else
+    std::cerr << "Cannot open file: " << input_file_name << std::endl;
+}
+
+void reconstruct_flat_zone(cv::Mat& input_image, cv::Mat& output_image, cv::Point pixel, int flat_zone_label) {
+  uchar pixel_label = input_image.at<uchar>(pixel);
+  output_image.at<uchar>(pixel) = flat_zone_label;
+
+  std::queue<cv::Point> flatzone;
+  flatzone.push(pixel);
+
+  while (!flatzone.empty()) {
+    cv::Point current_point = flatzone.front();
+    flatzone.pop();
+    for (cv::Point neighbour : get_neighbour(&input_image, current_point)) {
+      // IF ((I(p') == I(p)) AND (I'(p') != LABEL_FZ))
+      if (input_image.at<uchar>(neighbour) == pixel_label && output_image.at<uchar>(neighbour) != flat_zone_label) {
+        // Pixel p' belongs to x's flat zone and has not been treated
+        output_image.at<uchar>(neighbour) = flat_zone_label;
+        flatzone.push(neighbour);
+      }
+    }
+  }
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    std::cout << "Error: number of arguments: input output" << std::endl;
+  if (argc != 4) {
+    std::cerr << "Error: number of arguments: check documentation" << std::endl;
     return 1;
   }
-  cv::Mat img1 = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
 
-  cv::Mat output_img = something(&img1);
+  int x, y, connectivity, flat_zone_label = 0;
+  read_input(argv[1], x, y, connectivity, flat_zone_label);
+  cv::Mat input_img = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
+  cv::Mat output_img = cv::Mat::zeros(input_img.size(), input_img.type());
 
-  cv::imwrite(argv[2], output_img);
-  cv::imshow("Window: Output", output_img);
+  reconstruct_flat_zone(input_img, output_img, cv::Point(x, y), flat_zone_label);
+
+  cv::imwrite(argv[3], output_img);
 }
