38f1f786a47ebde3ce3664fc8c68ff3dfb46b1e2
Pablo Bande <pablobande200@gmail.com>
2023-04-11 01:04:14 +0100

    feat: Flat zone counter implemented

--- 12/README.md
+++ 12/README.md
@@ -1,14 +1,22 @@
-# Instructions
+# UPM Master's in Data Science: Exercises for Image Processing, Analysis and Classification subject
 
-## Compilation
+## Collaborators
+* Pablo Bande Sánchez - Girón ([@pbande](https://github.com/pbande))
+* Gabriela Argüelles Terrón ([@gabyarte](https://github.com/gabyarte))
+
+## Instructions
+
+### Compilation
 
 ```bash
 cmake .
 make
 ```
 
-## Run
+### Run
 
 ```bash
+./bin/exercise_12a data/test_1.txt data/immed_gray_inv_20051218_thresh127.pgm output/test_1.txt  
 
+./bin/exercise_12a data/test_1.txt data/immed_gray_inv.pgm output/test_2.txt 
 ```
--- 12/src/exercise_12a.cpp
+++ 12/src/exercise_12a.cpp
@@ -1,21 +1,80 @@
+#include <fstream>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <queue>
 
-cv::Mat something(cv::Mat* img) {
-  cv::Mat output_img = img->clone();
+#include "../../include/functions.cpp"
 
-  return output_img;
+void read_input(std::string input_file_name, int& connectivity) {
+  std::ifstream input(input_file_name);
+
+  if (input.is_open()) {
+    input >> connectivity;
+    input.close();
+  } else {
+    std::cerr << "Cannot open file: " << input_file_name << std::endl;
+  }
+}
+
+// Same function from exercise 11 but using int because uchar gave problems with 32 bits image
+void reconstruct_flat_zone(cv::Mat& input_image, cv::Mat& output_image, cv::Point pixel, int flat_zone_label) {
+  int pixel_label = static_cast<int>(input_image.at<uchar>(pixel));
+  output_image.at<int>(pixel) = flat_zone_label;
+
+  std::queue<cv::Point> flatzone;
+  flatzone.push(pixel);
+
+  while (!flatzone.empty()) {
+    cv::Point current_point = flatzone.front();
+    flatzone.pop();
+    for (cv::Point neighbour : get_neighbour(&input_image, current_point)) {
+      // IF ((I(p') == I(p)) AND (I'(p') != LABEL_FZ))
+      if (static_cast<int>(input_image.at<uchar>(neighbour)) == pixel_label && output_image.at<int>(neighbour) != flat_zone_label) {
+        // Pixel p' belongs to x's flat zone and has not been treated
+        output_image.at<int>(neighbour) = flat_zone_label;
+        flatzone.push(neighbour);
+      }
+    }
+  }
+}
+
+int get_num_of_flat_zones(cv::Mat& input_image) {
+  cv::Mat aux = cv::Mat::zeros(input_image.size(), CV_32S);
+  int flat_zone_label = 1;  // one diff color for each flat zone
+
+  for (int row = 0; row < input_image.rows; row++) {
+    for (int col = 0; col < input_image.cols; col++) {
+      if (aux.at<int>(row, col) == 0) {  // if not in flat zone yet
+        reconstruct_flat_zone(input_image, aux, cv::Point(col, row), flat_zone_label);
+        flat_zone_label++;
+      }
+    }
+  }
+
+  return flat_zone_label - 1;
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    std::cout << "Error: number of arguments: input output" << std::endl;
+  if (argc != 4) {
+    std::cerr << "Error: Incorrect number of arguments: check documentation" << std::endl;
     return 1;
   }
-  cv::Mat img1 = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
 
-  cv::Mat output_img = something(&img1);
+  int connectivity;
+  read_input(argv[1], connectivity);
 
-  cv::imwrite(argv[2], output_img);
-  cv::imshow("Window: Output", output_img);
+  cv::Mat input_image = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
+  int num_flat_zones = get_num_of_flat_zones(input_image);
+
+  std::cout << "Number of flat zones: " << num_flat_zones << std::endl;
+  std::ofstream output(argv[3]);
+  if (output.is_open()) {
+    output << num_flat_zones << std::endl;
+    output.close();
+  } else {
+    std::cerr << "Error: Cannot open output file" << std::endl;
+    return 1;
+  }
+
+  return 0;
 }
